b726ec60946a978462278471147ecc68cbd89da1
Nick Brown <nicholas.g.brown@gmail.com>
2019-02-09 14:42:54 -0500

    Added empty home.html

---
+++ people/src/main/java/com/demo/people/controllers/PeopleController.java
@@ -0,0 +1,13 @@
+package com.demo.people.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PeopleController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "home";
+    }
+}
